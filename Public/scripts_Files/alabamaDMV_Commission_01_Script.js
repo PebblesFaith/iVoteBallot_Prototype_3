ecbff0c7de0c6c4c747407cc81cd81497ec6b44d
Sarai Hannah Ajai <ajai.sarai.h@gmail.com>
2023-11-10 12:24:30 -0600

    Update alabamaDMV_Commission_01_Script.js
    Sarai Hannah Ajai have added two JavaScript coded languages by way of creating two getElementByIds for the "middleName" and "lastName" within the "Public/scripts_Files/alabamaDMV_Commission_01_Scripts.js" file dated on 11-10-23.

--- Public/scripts_Files/alabamaDMV_Commission_01_Script.js
+++ Public/scripts_Files/alabamaDMV_Commission_01_Script.js
@@ -5,7 +5,9 @@ const validateAlabamaDMV_CommissionForm = (e) => {
 
 	console.log('Sarai Hannah Ajai is testing her written JavaScript programmatic codes; in order to validate her contact us form has opened up correctly within the HTML webpage.')
 	
-	const userDMV_FirstName = document.getElementById('firstName').value.trim();                
+	const userDMV_FirstName = document.getElementById('firstName').value.trim(); 
+	const userDMV_MiddleName = document.getElementById('middleName').value.trim();  
+	const userDMV_LastName = document.getElementById('lastName').value.trim();               
 	             
 	                                
 	
@@ -13,50 +15,53 @@ const validateAlabamaDMV_CommissionForm = (e) => {
 	//const regExEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;                                           
 									   
 	if (userDMV_FirstName === '' || userDMV_FirstName == null || userDMV_FirstName.length <= 2 || !userDMV_FirstName.match(regExName)) {                                                               
-		document.getElementById('userDMV_FirstNameErrorMessage').innerHTML = 'The user first name is a required input field or you must enter more than two characters into the first name required input field.';
+		document.getElementById('userDMV_FirstNameErrorMessage').innerHTML = 'The user first name is a required input field or you must enter more than two characters into the userfirst name required input field.';
 	
 		setTimeout(function() {
 			document.getElementById('userDMV_FirstNameErrorMessage').innerHTML = '';
 		},
 		9000)
 	
-		return false;
-							
+		return false;		
+
 	}
-	/*
-	if (userMiddleName === '' || userMiddleName == null) {
-		document.getElementById('userMiddleNameErrorMessage').innerHTML = 'Do you have a middle name? If so, please type in your middle name into the input field or skip the middle name input field.';
+
+	if (userDMV_MiddleName === '' || userDMV_MiddleName == null) {
+		document.getElementById('userDMV_MiddleNameErrorMessage').innerHTML = 'Do the user have a middle name? If so, please type in the user middle name into the input field or skip the user middle name input field entirety.';
 	
 		setTimeout(function() {
-			document.getElementById('userMiddleNameErrorMessage').innerHTML = '';
+			document.getElementById('userDMV_MiddleNameErrorMessage').innerHTML = '';
 		},
 		9000)                                           
 							
-		} else if (userMiddleName.length <= 2 || !userMiddleName.match(regExName)) {
-		document.getElementById('userMiddleNameErrorMessage').innerHTML = 'Your middle name is an optional input field; however, you must enter more than two characters into the middle name required input field.';
+		} else if (userDMV_MiddleName.length <= 2 || !userDMV_MiddleName.match(regExName)) {
+		document.getElementById('userDMV_MiddleNameErrorMessage').innerHTML = 'The user middle name is an optional input field; however, you must enter more than two characters into the user middle name required input field.';
 	
 		setTimeout(function() {
-			document.getElementById('userMiddleNameErrorMessage').innerHTML = '';
+			document.getElementById('userDMV_MiddleNameErrorMessage').innerHTML = '';
 		},
 		9000)
 	
 		return false;  
 	
-	}                                  
+	} 
 	
-	if (userLastName === '' || userLastName == null || userLastName.length <= 2 || !userLastName.match(regExName)) {
-		document.getElementById('userLastNameErrorMessage').innerHTML = 'Your last name is a required input field or you must enter more than two characters into the last name required input field.';
+	if (userDMV_LastName === '' || userDMV_LastName == null || userDMV_LastName.length <= 2 || !userDMV_LastName.match(regExName)) {
+		document.getElementById('userDMV_LastNameErrorMessage').innerHTML = 'The user last name is a required input field or you must enter more than two characters into the user last name required input field.';
 	
 		setTimeout(function() {
-			document.getElementById('userLastNameErrorMessage').innerHTML = '';
+			document.getElementById('userDMV_LastNameErrorMessage').innerHTML = '';
 		},
 		9000)
 	
 		return false;
 	
 	}
-	
-	   */                       
+
+
+
+
+                     
 																
 				                                                                  
 	
