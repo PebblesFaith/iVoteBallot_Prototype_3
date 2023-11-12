7b969693bf4a40a25a7b417854ae6792de904656
Sarai Hannah Ajai <ajai.sarai.h@gmail.com>
2023-11-11 19:44:59 -0600

    Update alabamaDMV_Commission_01_Script.js
    Sarai Hannah Ajai have added one JavaScript coded languages by way of creating one constant; in order to getElementByIds using the DOM for the "address" within the "Public/scripts_Files/alabamaDMV_Commission_01_Scripts.js" file dated on 11-11-23.

--- Public/scripts_Files/alabamaDMV_Commission_01_Script.js
+++ Public/scripts_Files/alabamaDMV_Commission_01_Script.js
@@ -1,6 +1,5 @@
 
 
-
 const validateAlabamaDMV_CommissionForm = (e) => {    
 
 	console.log('Sarai Hannah Ajai is testing her written JavaScript programmatic codes; in order to validate her contact us form has opened up correctly within the HTML webpage.')
@@ -16,12 +15,15 @@ const validateAlabamaDMV_CommissionForm = (e) => {
 	const userDMV_SSN = document.getElementById('ssn').value.trim(); 
 	const userDMV_Email = document.getElementById('email').value.trim();
 	const userDMV_ConfirmEmail = document.getElementById('confirmEmail').value.trim();  
-	const userDMV_PhoneNumber = document.getElementById('phoneNumber').value.trim();		
+	const userDMV_PhoneNumber = document.getElementById('phoneNumber').value.trim();	
+	const userDMV_Address = document.getElementById('address').value.trim();	
 	
 	const regExName = /^[A-Za-z\s]+$/;
 	const regExSSN =  /^\d{3}\-\d{2}\-\d{4}$/;  
 	const regExEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;      
-	const regExPhoneNumber = /^\d{3}\-\d{3}\-\d{4}$/;                                      
+	const regExPhoneNumber = /^\d{3}\-\d{3}\-\d{4}$/;  
+	//const regExAddress = /^\d+\s[\d\w]+\s(.*)$/;  
+	const regExAddress = /^\d+[ ](?:[A-Za-z0-9.-]+[ ]?)+(?:Avenue|Alley|Anex|Arcade|Boyou|Beach|Bend|Bluffs|Bottom|Branch|Brook|Bridge|Burg|Burgs|Bypass|Camp|Canyon|Cape|Causeway|Parkway|Pkwy|Center|Centers|Circle|Circles|Cliff|Cliffs|Club|Corner|Cove|Creek|Cresent|Crest|Crossing|XING|Dale|Dam|Divide|Estate|Expressway|Expy|Express|Fall|Falls|Ferry|Field|Fields|Forest|Fork|Freeway|Garden|Gardens|Gateway|Glen|Glens|Greens|Grove|Harbor|Harbors|Haven|Heights|Highway|Hill|Hills|Hollow|Inlet|Island|Islands|Isle|Junction|Key|Keys|Quay|Knoll|Knolls|Lake|Lakes|Land|Landing|Lock|Locks|Lodge|Loop|Mall|Manor|Manors|Meadow|Meadows|Mews|Mill|Mills|Mission|Motorway|Mountain|Mountains|Neck|Orchard|Oval|Overpass|Park|Parks|Parkways|Pass|Passage|Path|Pike|Plaza|Port|Ports|Ramp|Ranch|Ridge|River|Route|Row|Run|Shoal|Shore|Shores|Skyway|Springs|Springs|Square|Squares|SQ|Station|Stravenue|STRA|Stream|Summit|SMT|Terrace|TRCE|Throughway|TRWY|Trail|TRL|Tunnel|TUNL|Turnpike|TPKE|Underpass|UPAS|Union|UN|Unions|UNS|Valley|VLY|Viaduct|VIA|Village|View|Views|VW|Ville|Vista|VL|VIS|Walk|Walks|Way|Ways|Well|Wells|WL|WLS|Lane|Road|Boulevard|Drive|Street|Place|Ave|Dr|Rd|Blvd|Ln|St|Cir|South|S|North|N|West|W|East|E)\.?/;                                
 									   
 	if (userDMV_FirstName === '' || userDMV_FirstName == null || userDMV_FirstName.length <= 2 || !userDMV_FirstName.match(regExName)) {                                                               
 		document.getElementById('userDMV_FirstNameErrorMessage').innerHTML = 'The user first name is a required input field or you must enter more than two characters into the userfirst name required input field.';
@@ -188,6 +190,18 @@ const validateAlabamaDMV_CommissionForm = (e) => {
 
 		setTimeout(function() {
 			document.getElementById('userDMV_PhoneNumberErrorMessage').textContent = '';
+		},
+		9000)
+
+		return false;				
+
+	}	
+
+	if (userDMV_Address === '' || userDMV_Address == null || userDMV_Address.length <= 6 || !userDMV_Address.match(regExAddress))  {                                                               
+		document.getElementById('userDMV_AddressErrorMessage').textContent = 'The user\s street address is a required field and you must enter the user correct street address format into the require input field.';
+
+		setTimeout(function() {
+			document.getElementById('userDMV_AddressErrorMessage').textContent = '';
 		},
 		9000)
 
