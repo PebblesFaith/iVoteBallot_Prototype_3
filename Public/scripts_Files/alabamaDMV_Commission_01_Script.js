4912b2a983ceb6172b6008cc5be899886fe5c313
Sarai Hannah Ajai <ajai.sarai.h@gmail.com>
2023-11-30 17:01:24 -0600

    Update alabamaDMV_Commission_01_Script.js
    Sarai Hannah Ajai have made updates to the "Public/scripts_Files/alabamaDMV_Commission_01_Script.js" file dated on 11-30-23 at approximately 05:01pm.

--- Public/scripts_Files/alabamaDMV_Commission_01_Script.js
+++ Public/scripts_Files/alabamaDMV_Commission_01_Script.js
@@ -29,7 +29,6 @@ const validateAlabamaDMV_CommissionForm = (e) => {
     const userCommissionIvoteBallotIdIdentifierCode = document.getElementById('iVoteBallotIdIdentifierCode').value.trim();
     const userCommissionIvoteBallotIdCodeBcryptic = document.getElementById('iVoteBallotIdCodeHidden_Bcryptic').value.trim();
 	
-	
 	const regExName = /^[A-Za-z\s]+$/;
 	const regExSSN =  /^\d{3}\-\d{2}\-\d{4}$/;  
 	const regExEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;      
