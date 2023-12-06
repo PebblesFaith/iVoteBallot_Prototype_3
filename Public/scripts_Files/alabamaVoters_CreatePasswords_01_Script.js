fe771235dd58505ad1a558d14bf77236e70dedb3
Sarai Hannah Ajai <ajai.sarai.h@gmail.com>
2023-12-06 17:21:05 -0600

    Update alabamaVoters_CreatePasswords_01_Script.js
    Sarai Hannah Ajai have updated the "Public/scripts_Files/alabamaVoters_CreatePasswords_01_Script.js" file dated on 12-06-23 at approximately 5:20pm.

--- Public/scripts_Files/alabamaVoters_CreatePasswords_01_Script.js
+++ Public/scripts_Files/alabamaVoters_CreatePasswords_01_Script.js
@@ -1,16 +1,37 @@
 
 
+function toggle_InputPasswordCheckBox() {
+	var passwordInput = document.getElementById("password");
+	var toggleButton = document.getElementById("showHide_01");
 
+	if (passwordInput.type === "password") {
+	  passwordInput.type = "text";
+	  toggleButton.textContent = "Hide";
+	} else {
+	  passwordInput.type = "password";
+	  toggleButton.textContent = "Show";
+	}
+}
 
+function toggle_InputConfirmPasswordCheckBox() {
+	var confirmPasswordInput = document.getElementById("confirmPassword");
+	var toggleButton = document.getElementById("showHide_02");
 
+	if (confirmPasswordInput.type === "password") {
+	  confirmPasswordInput.type = "text";
+	  toggleButton.textContent = "Hide";
+	} else {
+	  confirmPasswordInput.type = "password";
+	  toggleButton.textContent = "Show";
+	}
+}
 
 const validateCreatePasswordsForm = (e) => {
 
 	const userPassword = document.getElementById('password').value.trim();
 	const userConfirmPassword = document.getElementById('confirmPassword').value.trim();
 
-	const regExPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,32}$/;
-	
+	const regExPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,32}$/;	
 	
 	if (userPassword === '' || userPassword == null || userPassword.length <= 7 || !userPassword.match(regExPassword)) {
 		document.getElementById('userPasswordErrorMessage').textContent = 'Your password is a required input field.';	
@@ -47,8 +68,7 @@ const validateCreatePasswordsForm = (e) => {
 		return false;
 
 	}	
-
-
+	
 		e.preventDefault();
 		form.addEventListener('submit', validateCreatePasswordsForm)
 
