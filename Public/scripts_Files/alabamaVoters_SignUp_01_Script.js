8a9697aefa5662979eda22884f8ba3023596e94a
Sarai Hannah Ajai <ajai.sarai.h@gmail.com>
2023-12-20 12:24:28 -0600

    Update alabamaVoters_SignUp_01_Script.js
    Sarai Hannah Ajai has excluded the "user" terms from the name attributes to address issues arising from the deserialization call function in the session cookie ID. This function authorizes the name attribute values from the SQLite3 database, preventing user syntax errors caused by referencing the previous NPM project within the name attributes. This peculiar occurrence was observed on December 20, 2023, at around 12:24pm.

--- Public/scripts_Files/alabamaVoters_SignUp_01_Script.js
+++ Public/scripts_Files/alabamaVoters_SignUp_01_Script.js
@@ -3,13 +3,13 @@ const  validateAlabamaVotersSignUpForm = (e) => {
 
 	console.log('Sarai Hannah Ajai is testing her written JavaScript programmatic codes; in order to validate her \'validateAlabamaVotersSignUpForm()\' form has opened up correctly within the HTML webpage.')
 	
-	const userDMVEmail = document.getElementById('email').value.trim();
-	const userIvoteBallotIdIdentifierCode = document.getElementById('iVoteBallotIdIdentifierCode').value.trim();
+	const DMVEmail = document.getElementById('email').value.trim();
+	const IvoteBallotIdIdentifierCode = document.getElementById('iVoteBallotIdIdentifierCode').value.trim();
 
 	const regExEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;      
 	const regIvoteballotIdIdentifierCode = /\d{2}[A-Z]{1}\d{1}\-\d{1}[A-Z]{1}\d{1}\-[A-Z]{2}\-\d{3}[A-Z]{2}\d{6}\-\d{2}/;
 	
-	if (userDMVEmail === '' || userDMVEmail == null || userDMVEmail.length <= 6 || !userDMVEmail.match(regExEmail)) {
+	if (DMVEmail === '' || DMVEmail == null || DMVEmail.length <= 6 || !DMVEmail.match(regExEmail)) {
 		document.getElementById('userDMVEmailErrorMessage').textContent = 'The user\'s email address is a required input field and you must enter the correct user\s email address format into the require input field.';
 
 		setTimeout(function() {
@@ -21,7 +21,7 @@ const  validateAlabamaVotersSignUpForm = (e) => {
 
 	}
 
-	if ( userIvoteBallotIdIdentifierCode === '' || userIvoteBallotIdIdentifierCode  == null || !userIvoteBallotIdIdentifierCode.match(regIvoteballotIdIdentifierCode)) {                                                               
+	if (IvoteBallotIdIdentifierCode === '' || IvoteBallotIdIdentifierCode  == null || !IvoteBallotIdIdentifierCode.match(regIvoteballotIdIdentifierCode)) {                                                               
         document.getElementById('useriVoteBallotIdIdentifierCodeErrorMessage').textContent = 'The user\'s iVoteBallot Id Identifier is a required input field and you must enter the user correct alphanumeric characters format into the required input field.';
 
         setTimeout(function() {
