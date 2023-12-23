24cff482e1df76acf0111f965b039fc39f4597f3
Sarai Hannah Ajai <ajai.sarai.h@gmail.com>
2023-12-23 15:05:31 -0600

    Update alabamaVoters_VerifyEmailPassword_01_Script.js
    Sarai Hannah Ajai have updated to the "Public/views/alabamaVoters_VerifyEmailPassword_01.ejs" dated on 12-23-23 at approximately 03:04pm.

--- Public/scripts_Files/alabamaVoters_VerifyEmailPassword_01_Script.js
+++ Public/scripts_Files/alabamaVoters_VerifyEmailPassword_01_Script.js
@@ -3,7 +3,7 @@ const validateAlabamaVotersVerifyTempPasswordForm = (e) => {
 	console.log('Sarai Hannah Ajai is testing her written JavaScript programmatic codes; in order to validate her \'validateAlabamaVotersSignUpForm()\' form has opened up correctly within the HTML webpage.')
 	
 	const DMVEmail = document.getElementById('email').value.trim();
-	const temporary_Password = document.getElementById('temporary_Password').value.trim();
+	const Temporary_Password = document.getElementById('temporary_Password').value.trim();
 
 	const regExEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;      
 	const regExPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,32}$/;
@@ -20,7 +20,7 @@ const validateAlabamaVotersVerifyTempPasswordForm = (e) => {
 
 	}
 	
-	if (temporary_Password === '' || temporary_Password == null || temporary_Password.length <= 7 || !temporary_Password.match(regExPassword)) {
+	if (Temporary_Password === '' || Temporary_Password == null || Temporary_Password.length <= 7 || !Temporary_Password.match(regExPassword)) {
 		document.getElementById('userTemporary_PasswordErrorMessage').textContent = 'Your temporary password is a required input field.';
 
 		setTimeout(function() {
