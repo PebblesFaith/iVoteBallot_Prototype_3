cafe4bfaeee19b32854160c293fb17f8d3dec43b
Sarai Hannah Ajai <ajai.sarai.h@gmail.com>
2024-03-29 13:09:49 -0500

    Create hrm_Login_01_Script.js
    Sarai Hannah Ajai had created the "Public/scripts_Files/hrm_Login_01_Scripts.js" file dated on 03-29-24 at approximately 01:09pm.

---
+++ Public/scripts_Files/hrm_Login_01_Script.js
@@ -0,0 +1,55 @@
+function toggle_InputPasswordCheckBox() {
+	var passwordInput = document.getElementById("password");
+	var toggleButton = document.getElementById("showHide_01");
+
+	if (passwordInput.type === "password") {
+	  passwordInput.type = "text";
+	  toggleButton.textContent = "Hide";
+	} else {
+	  passwordInput.type = "password";
+	  toggleButton.textContent = "Show";
+	}
+}
+
+const validateAlabamaVotersLogInForm = (e) => { 	
+
+	console.log('Sarai Hannah Ajai is testing her written JavaScript programmatic codes; in order to validate her \'validateAlabamaVotersSignUpForm()\' form has opened up correctly within the HTML webpage.')
+	
+	const DMVEmail = document.getElementById('email').value.trim();
+	const Password = document.getElementById('password').value.trim();
+
+	const regExEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;      
+	const regExPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,32}$/;
+	
+	if (DMVEmail === '' || DMVEmail == null || DMVEmail.length <= 6 || !DMVEmail.match(regExEmail)) {
+		document.getElementById('userDMVEmailErrorMessage').textContent = 'The user\'s email address is a required input field and you must enter the correct user\s email address format into the require input field.';
+
+		setTimeout(function() {
+			document.getElementById('userDMVEmailErrorMessage').textContent = '';
+		},
+		9000)
+
+		return false;
+
+	}
+	
+	if (Password === '' || Password == null || Password.length <= 7 || !Password.match(regExPassword)) {
+		document.getElementById('userPasswordErrorMessage').textContent = 'Your password is a required input field.';
+
+		setTimeout(function() {
+			document.getElementById('userPasswordErrorMessage').textContent = '';
+		},
+		9000)
+	
+		return false;
+	
+	}     
+
+	e.preventDefault();  
+				
+	form.addEventListener('submit', validateAlabamaVotersLogInForm);
+
+	return true;     		
+
+}
+
