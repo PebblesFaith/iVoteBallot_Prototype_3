87463bc886bb8f052ec6d49a884d55c7c2a9aa27
Sarai Hannah Ajai <ajai.sarai.h@gmail.com>
2024-04-02 18:34:35 -0500

    Create hrm_SignUp_01_Script.js
    Sarai Hannah Ajai have created the "Public/scrips_Files/hrm_SignUp_01_Script.js" file dated on 04-02-24 at approximately 06:34pm.

---
+++ Public/scripts_Files/hrm_SignUp_01_Script.js
@@ -0,0 +1,54 @@
+function toggle_InputPasswordCheckBox() {
+	var passwordInput = document.getElementById("iVoteBallotIdIdentifierCode");
+	var toggleButton = document.getElementById("showHide_01");
+
+	if (passwordInput.type === "password") {
+	  passwordInput.type = "text";
+	  toggleButton.textContent = "Hide";
+	} else {
+	  passwordInput.type = "password";
+	  toggleButton.textContent = "Show";
+	}
+}
+
+const  validateHRMSignUpForm = (e) => { 	
+
+	console.log('Sarai Hannah Ajai is testing her written JavaScript programmatic codes; in order to validate her \'validateAlabamaVotersSignUpForm()\' form has opened up correctly within the HTML webpage.')
+	
+	const EmployeeEmail = document.getElementById('email').value.trim();
+	const EmployeeId = document.getElementById('id').value.trim();
+
+	const regExEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;      
+	const regExId = /\d{2}[A-Z]{1}\d{1}\-\d{1}[A-Z]{1}\d{1}\-[A-Z]{2}\-\d{3}[A-Z]{2}\d{6}\-\d{2}/;
+	
+	if (EmployeeEmail === '' || EmployeeEmail == null || EmployeeEmail.length <= 6 || !EmployeeEmail.match(regExEmail)) {
+		document.getElementById('userEmployeeEmailErrorMessage').textContent = 'Error Message: Your email address is required. Please enter the email address in the correct format into the required input field.';
+
+		setTimeout(function() {
+			document.getElementById('userEmployeeEmailErrorMessage').textContent = '';
+		},
+		9000)
+
+		return false;
+
+	}
+
+	if (EmployeeId === '' || EmployeeId  == null || !EmployeeId.match(regExId)) {                                                               
+        document.getElementById('userEmployeeIdErrorMessage').textContent = 'Error Message: Your employment Id code is a required';
+
+        setTimeout(function() {
+            document.getElementById('userEmployeeIdErrorMessage').textContent = '';
+        },
+        9000)
+
+        return false;					
+                            
+    } 	       
+	
+	e.preventDefault();  
+				
+	form.addEventListener('submit', validateHRMSignUpForm);
+
+	return true;     		
+
+}
