21fd3d6420948d7f31330344aa05a6f6be98b7a4
Sarai Hannah Ajai <ajai.sarai.h@gmail.com>
2024-04-02 20:58:54 -0500

    Update hrm_SignUp_01_Script.js
    Sarai Hannah Ajai have updated the "Public/scripts_Files/hrm_SignUp_01_Script.js" file dated on 04-02-24 at approximately 08:58pm.

--- Public/scripts_Files/hrm_SignUp_01_Script.js
+++ Public/scripts_Files/hrm_SignUp_01_Script.js
@@ -19,7 +19,7 @@ const  validateHRMSignUpForm = (e) => {
 	const EmployeeId = document.getElementById('id').value.trim();
 
 	const regExEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;      
-	const regExId = /\d{2}[A-Z]{1}\d{1}\-\d{1}[A-Z]{1}\d{1}\-[A-Z]{2}\-\d{3}[A-Z]{2}\d{6}\-\d{2}/;
+	const regExPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,32}$/;
 	
 	if (EmployeeEmail === '' || EmployeeEmail == null || EmployeeEmail.length <= 6 || !EmployeeEmail.match(regExEmail)) {
 		document.getElementById('userEmployeeEmailErrorMessage').textContent = 'Error Message: Your email address is required. Please enter the email address in the correct format into the required input field.';
@@ -33,7 +33,7 @@ const  validateHRMSignUpForm = (e) => {
 
 	}
 
-	if (EmployeeId === '' || EmployeeId  == null || !EmployeeId.match(regExId)) {                                                               
+	if (EmployeeId === '' || EmployeeId  == null || !EmployeeId.match(regExPassword)) {                                                               
         document.getElementById('userEmployeeIdErrorMessage').textContent = 'Error Message: Your employment Id code is a required';
 
         setTimeout(function() {
